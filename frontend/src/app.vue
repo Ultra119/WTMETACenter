d6b6a60acd50561e62c7d64489534f247451e9d7
Ultra119 <64977575+Ultra119@users.noreply.github.com>
2026-06-09 14:35:38 +0300

    remove loading animation

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,10 +1,5 @@
 <template>
   <v-app theme="wt">
-    <v-overlay :model-value="store.loading" persistent class="align-center justify-center">
-      <v-progress-circular indeterminate size="64" color="primary" />
-      <div class="mt-4 text-caption text-medium-emphasis">{{ t('common.loading') }}</div>
-    </v-overlay>
-
     <Transition name="filter-bar-fade">
       <div v-if="store.filtering" class="filter-progress-bar">
         <div class="filter-progress-inner" />
